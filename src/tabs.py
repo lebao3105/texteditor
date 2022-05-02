bf5f4ff947f33f8aeb1cc7c1c25196b141abc71e
lebao3105 <bao12345yocoo@gmail.com>
2022-05-02 21:20:44 +0700

    Update message

--- src/tabs.py
+++ src/tabs.py
@@ -6,7 +6,7 @@ _ = gettext.gettext
 def add_tab(self):
     tab_name = self.notebook.index("end") + 1
     new_tab = Frame(self.notebook)
-    self.notebook.add(new_tab, text=_("Untitled") + str(tab_name))
+    self.notebook.add(new_tab, text=_("Untitled ") + str(tab_name))
     place_textbox(new_tab)
     self.notebook.select(tab_name)
     self.tab_name_entry.delete(0, END)
@@ -28,5 +28,5 @@ def tabs_close(self):
     # If not, just close the selected tab and keep
     else:
         index = str(self.notebook.index(self.notebook.select()))
-        print(_("Closing tab...") + index)
+        print(_("Closing tab ") + index + "...")
         self.notebook.forget(self.notebook.select())
