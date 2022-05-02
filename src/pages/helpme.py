be7fd8787f98474b17a637db2fea3bdb54244012
lebao3105 <bao12345yocoo@gmail.com>
2022-05-02 15:34:32 +0700

    The language resource updator

--- src/pages/helpme.py
+++ src/pages/helpme.py
@@ -18,7 +18,7 @@ def place_texts(self):
     self.label_help = Label(self, text=_("Help"))
     self.label_help_text = Label(self, text=_("How to use this application?"))
     self.label_help_text_2 = Label(self, text=_("Just do what you need like any other text editor,"))
-    self.label_help_text_3 = Label(self, text=_("but no tabs support."))
+    self.label_help_text_3 = Label(self, text=_("with tabs support."))
     self.label_help.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
     self.label_help_text.grid(row=1, column=0, sticky="ew", padx=5)
     self.label_help_text_2.grid(row=2, column=0, sticky="ew", padx=5)
---
+++ upd_trans.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Shell script to update and generate new .pot file...
+
+if [[ $1 == "--updated-strings" || $1 == "-upd" ]]; then
+    echo "Generating new .mo files..."
+    for i in $(ls -d po/*/); do
+        msgfmt ${i%%/}/LC_MESSAGES/base -o ${i%%/}/LC_MESSAGES/base.mo
+    done
+    echo "Done!"
+    exit 0
+
+elif [[ $1 == "--new-template" || $1 == "-tep" ]]; then
+    echo "Generating new .pot file..."
+    xgettext -d base -o po/base.pot src/{main,setup,tabs}.py src/pages/*.py
+    for i in $(ls -d po/*/); do
+        msgmerge -U ${i%%/}/LC*/base.po po/base.pot
+    done
+    echo "Done!"
+
+elif [[ $1 == "--help" || $1 == "-h" ]]; then
+    echo "./upd_trans.sh [options]"
+    echo "--updated-strings | -upd : Re-generate .mo files"
+    echo "--help | -h : Show this help"
+    echo "--new-template | -tep : Re-generate .pot (translation template file)"
+    exit 1
+
+else
+    echo "Invalid option!"
+    echo "Use --help | -h to show available options you can use."
+    exit 1
+fi
