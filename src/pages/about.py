148154880d1a4702edcc43fa5f8143920440e759
lebao3105 <bao12345yocoo@gmail.com>
2022-05-15 20:45:37 +0700

    Update _

--- src/pages/about.py
+++ src/pages/about.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 import sys
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
@@ -6,18 +7,23 @@ from miscs import init, get_config
 
 def about_run(self):
     about_win = Toplevel(self)
-    about_win.title(_("About"))
+    about_win._ = ""
+    init.initialize(about_win, 1)
+    about_win.title(about_win._("About"))
     about_win.rowconfigure(0, weight=1)
     about_win.columnconfigure(0, weight=1)
     init.initialize(about_win, 1)
     place_texts(about_win)
 
 def place_texts(self):
-    name = Label(self, text=_("Text editor"))
-    version = Label(self, text=_("Version 1.0."))
-    author = Label(self, text=_("Author: ")+("Le Bao Nguyen"))
-    license_text = Label(self, text=_("License: ")+("GPLv3"))
+    test = ImageTk.PhotoImage(Image.open("data/org.lebao3105.texteditor.Devel.png"))
+    label_image = Label(self, image=test)
+    name = Label(self, text=self._("Text editor"))
+    version = Label(self, text=self._("Version 1.0."))
+    author = Label(self, text=self._("Author: ")+("Le Bao Nguyen"))
+    license_text = Label(self, text=self._("License: ")+("GPLv3"))
     
+    label_image.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
     name.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
     version.grid(row=2, column=0, sticky="ew", padx=5)
     author.grid(row=3, column=0, sticky="ew", padx=5)
--- src/pages/helpme.py
+++ src/pages/helpme.py
@@ -1,13 +1,14 @@
 from tkinter import *
-import gettext
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from miscs import init, get_config
 
-vi = gettext.translation('base', localedir='po', languages=['vi'])
-vi.install()
-_ = vi.gettext
-    
 def help_run(self):
     help_win = Toplevel(self)
-    help_win.title(_("Help"))
+    help_win._ = ""
+    init.initialize(help_win, 1)
+    help_win.title(help_win._("Help"))
     help_win.geometry("300x300")
     help_win.rowconfigure(0, weight=1)
     help_win.columnconfigure(0, weight=1)
@@ -16,11 +17,16 @@ def help_run(self):
     place_texts(help_win)
 
 def place_texts(self):
-    self.label_help = Label(self, text=_("Help"))
-    self.label_help_text = Label(self, text=_("How to use this application?"))
-    self.label_help_text_2 = Label(self, text=_("Just do what you need like any other text editor,"))
-    self.label_help_text_3 = Label(self, text=_("with tabs support."))
+    self.label_help = Label(self, text=self._("Help"))
+    self.label_help_text = Label(self, text=self._("How to use this application?"))
+    self.label_help_text_2 = Label(self, text=self._("Just do what you need like any other text editor,"))
+    self.label_help_text_3 = Label(self, text=self._("with tabs support."))
     self.label_help.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
     self.label_help_text.grid(row=1, column=0, sticky="ew", padx=5)
     self.label_help_text_2.grid(row=2, column=0, sticky="ew", padx=5)
     self.label_help_text_3.grid(row=3, column=0, sticky="ew", padx=5)
+    init.initialize(self, 2)
+    get_config.change_text_color(self.label_help)
+    get_config.change_text_color(self.label_help_text)
+    get_config.change_text_color(self.label_help_text_2)
+    get_config.change_text_color(self.label_help_text_3)
