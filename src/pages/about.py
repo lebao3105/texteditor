3dc5bf851ba95f02f3cd4a7438414d050b15d393
lebao3105 <bao12345yocoo@gmail.com>
2022-05-23 12:29:14 +0700

    Test about page a bit

--- src/pages/about.py
+++ src/pages/about.py
@@ -15,18 +15,26 @@ def about_run(self):
     place_texts(about_win)
 
 def place_texts(self):
-    img = Label(self, image=self.p2)
+    frame = Frame(self)
+    img = Label(frame, image=self.p2)
+    header = Label(frame, text=self._("About this application"), font=("Arial", 16))
     name = Label(self, text=self._("Text editor"))
     version = Label(self, text=self._("Version 1.0."))
     author = Label(self, text=self._("Author: ")+("Le Bao Nguyen"))
     license_text = Label(self, text=self._("License: ")+("GPLv3"))
     
-    img.grid(row=0, column=0, rowspan=2, sticky=W+E+N+S)
-    name.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
-    version.grid(row=2, column=0, sticky="ew", padx=5)
-    author.grid(row=3, column=0, sticky="ew", padx=5)
-    license_text.grid(row=4, column=0, sticky="ew", padx=5)
+    # Place widgets
+    frame.grid(row=0, column=0, sticky="nsew", padx=20, pady=20)
+    img.grid(row=1, column=0)
+    header.grid(row=0, column=0)
+    name.grid(row=0, column=1, sticky="ew")
+    version.grid(row=1, column=1, sticky="ew")
+    author.grid(row=2, column=1, sticky="ew")
+    license_text.grid(row=3, column=1, sticky="ew")
     init.initialize(self, 2)
+
+    # Change text color
+    get_config.change_text_color(header)
     get_config.change_text_color(name)
     get_config.change_text_color(version)
     get_config.change_text_color(author)
