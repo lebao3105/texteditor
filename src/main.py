7e8fa599c6d64aa686b08c04d5b894e8d19d1c26
lebao3105 <bao12345yocoo@gmail.com>
2022-05-26 14:27:34 +0700

    Nothing

--- src/main.py
+++ src/main.py
@@ -96,7 +96,6 @@ def place_widgets(self):
     self.tab_right_click.add_command(label=self._("Close the current opening tab"), 
                                     accelerator="Ctrl+W",
                                     command=lambda: tabs.tabs_close(self))
-    self.bind("<Button-3>", lambda event: self.tab_right_click.post(event.x_root, event.y_root))
     self.bind("Button-release", lambda event: tabs.move_tabs(self, event))
     # Bind the function to the event
     binder(self)
