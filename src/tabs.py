a383c2db2ae17dcefca316ec322c749d0b7e5ba3
lebao3105 <bao12345yocoo@gmail.com>
2022-06-02 09:42:16 +0700

    A right click menu for the text editor

--- src/tabs.py
+++ src/tabs.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import miscs.init
+import miscs.file_operations
 
 # For example:
 # We have 2 tabs: Untitled and Untitled 2. You close Untitled
@@ -20,19 +21,20 @@ def checker(self):
     elif str(self.notebook.index("end") + 1) in find_tab:
         return False
 
-def add_tab(self, root):
+def add_tab(self):
     if checker(self):
         tab_name = self.notebook.index("end") + 1
     else:
         tab_name = self.notebook.index("end") + 2
     new_tab = Frame(self.notebook)
     self.notebook.add(new_tab, text=self._("Untitled ") + str(tab_name))
-    place_textbox(new_tab, root)
+    place_textbox(new_tab, self)
     self.notebook.select(new_tab)
 
 def place_textbox(self, root):
     # Text box
     self.text_editor = Text(self, wrap="word", undo=True, font=("Arial", 12))
+    self.text_editor.bind("<Button-3>", lambda event:place_right_click_menu(self, event, root))
     self.text_editor.pack(expand=True, fill="both")
     # Scrollbar
     self.scroll = ttk.Scrollbar(self.text_editor, orient="vertical", command=self.text_editor.yview)
@@ -40,7 +42,7 @@ def place_textbox(self, root):
     self.scroll.pack(side="right", fill="y")
     self.scroll2.pack(side="bottom", fill="x")
     self.text_editor.configure(yscrollcommand=self.scroll.set, xscrollcommand=self.scroll2.set, undo=True, autoseparators=True)
-    root.text_editor = self.text_editor
+    #root.text_editor = self.text_editor
     miscs.init.initialize(self.text_editor, 2)
 
 def tabs_close(self):
@@ -52,3 +54,17 @@ def tabs_close(self):
     # the main window open
     else:
         self.notebook.forget(self.notebook.select())
+
+def place_right_click_menu(self, event, root):
+    try:
+        m = Menu(self.text_editor, tearoff=0)
+        m.add_command(label=root._("Copy"), command=lambda:miscs.file_operations.copy(self))
+        m.add_command(label=root._("Paste"), command=lambda:miscs.file_operations.paste(self))
+        m.add_command(label=root._("Cut"), command=lambda:miscs.file_operations.cut(self))
+        m.add_separator()
+        m.add_command(label=root._("Save"), command=lambda:miscs.file_operations.save_file(self))
+        m.add_command(label=root._("Save as"), command=lambda:miscs.file_operations.save_as(self))
+        m.tk_popup(event.x_root, event.y_root)
+    finally:
+        m.grab_release()
+
