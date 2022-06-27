787c3413c4306d29f20a782b5b9befe9ea397d68
lebao3105 <bao12345yocoo@gmail.com>
2022-06-27 14:03:53 +0700

    Fixed not-working right click menu

--- src/tabs.py
+++ src/tabs.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import miscs.init
 import miscs.file_operations
+import miscs.constants as i
 
 # For example:
 # We have 2 tabs: Untitled and Untitled 2. You close Untitled
@@ -15,30 +16,32 @@ def checker(self):
     if str(self.notebook.index("end")) in find_tab:
         return True
     # if this is the first tab yet
-    elif find_tab == self._("Untitled "):
+    elif find_tab == self._(i.UNTITLED):
         return True
     # what I mentioned before
     elif str(self.notebook.index("end") + 1) in find_tab:
         return False
 
 def add_tab(self):
+    tabname = self._("Text Editor ") + " - "
     # check if there are no tabs yet
     if self.notebook.index("end") == 0:
-        # if there's no tab yet, create a new one
         firsttab = Frame(self.notebook)
-        self.notebook.add(firsttab, text=self._("Untitled "))
+        self.notebook.add(firsttab, text=self._(i.UNTITLED))
         # place the textbox
         place_textbox(firsttab, self)
         self.notebook.select(firsttab)
+        self.title(tabname + self._(i.UNTITLED))
     else:
         if checker(self):
-            tab_name = self.notebook.index("end") + 1
+            tab_name = self._(i.UNTITLED) + str(self.notebook.index("end") + 1)
         else:
-            tab_name = self.notebook.index("end") + 2
+            tab_name = self._(i.UNTITLED) + str(self.notebook.index("end") + 2)
         new_tab = Frame(self.notebook)
-        self.notebook.add(new_tab, text=self._("Untitled ") + str(tab_name))
+        self.notebook.add(new_tab, text=tab_name)
         place_textbox(new_tab, self)
         self.notebook.select(new_tab)
+        self.title(tabname + tab_name)
 
 def place_textbox(self, root):
     # Text box
@@ -46,12 +49,12 @@ def place_textbox(self, root):
     self.text_editor.bind("<Button-3><ButtonRelease-3>", lambda event:place_right_click_menu(self, event, root))
     self.text_editor.pack(expand=True, fill="both")
     # Scrollbar
+    root.text_editor = self.text_editor
     self.scroll = ttk.Scrollbar(self.text_editor, orient="vertical", command=self.text_editor.yview)
     self.scroll2 = ttk.Scrollbar(self.text_editor, orient="horizontal", command=self.text_editor.xview)
     self.scroll.pack(side="right", fill="y")
     self.scroll2.pack(side="bottom", fill="x")
     self.text_editor.configure(yscrollcommand=self.scroll.set, xscrollcommand=self.scroll2.set, undo=True, autoseparators=True)
-    root.text_editor = self.text_editor
     miscs.init.initialize(self.text_editor, 2)
 
 def tabs_close(self):
@@ -67,15 +70,15 @@ def tabs_close(self):
 def place_right_click_menu(self, event, root):
     try:
         m = Menu(self.text_editor, tearoff=0)
-        m.add_command(label=root._("Copy"), accelerator="Ctrl+C", event=event.widget.event_generate("<Control-c>"))
-        m.add_command(label=root._("Paste"), accelerator="Ctrl+V", event=event.widget.event_generate("<Control-v>"))
-        m.add_command(label=root._("Cut"), accelerator="Ctrl+X", event=event.widget.event_generate("<Control-x>"))
+        m.add_command(label=root._("Copy"), accelerator="Ctrl+C", command=lambda: root.event_generate("<Control-c>"))
+        m.add_command(label=root._("Paste"), accelerator="Ctrl+V", command=lambda: root.event_generate("<Control-v>"))
+        m.add_command(label=root._("Cut"), accelerator="Ctrl+X", command=lambda: root.event_generate("<Control-x>"))
         m.add_separator()
-        m.add_command(label=root._("Save"), accelerator="Ctrl+S", event=event.widget.event_generate("<Control-s>"))
-        m.add_command(label=root._("Save as"), accelerator="Ctrl+Shift+S", event=event.widget.event_generate("<Control-Shift-s>"))
+        m.add_command(label=root._("Save"), accelerator="Ctrl+S", command=lambda: root.event_generate("<Control-s>"))
+        m.add_command(label=root._("Save as"), accelerator="Ctrl+Shift+S", command=lambda: root.event_generate("<Control-Shift-s>"))
         m.add_separator()
-        m.add_command(label=root._("Undo"), accelerator="Ctrl+Z", event=event.widget.event_generate("<Control-z>"))
-        m.add_command(label=root._("Redo"), accelerator="Ctrl+Y", event=event.widget.event_generate("<Control-y>"))
+        m.add_command(label=root._("Undo"), accelerator="Ctrl+Z", command=lambda: root.event_generate("<Control-z>"))
+        m.add_command(label=root._("Redo"), accelerator="Ctrl+Y", command=lambda: root.event_generate("<Control-y>"))
         m.tk_popup(event.x_root, event.y_root)
     finally:
         m.grab_release()
