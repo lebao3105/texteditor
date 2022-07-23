9982f902b93c3a1c1dd8605471c92e842db91c67
lebao3105 <bao12345yocoo@gmail.com>
2022-07-23 11:32:18 +0700

    Use os.path.isfile instead or exists

--- texteditor/main.py
+++ texteditor/main.py
@@ -2,7 +2,7 @@ import sys
 import tabs
 import mainwindow
 from miscs import file_operations
-from os.path import exists
+from os.path import isfile
 
 if __name__ == '__main__':
     
@@ -10,14 +10,13 @@ if __name__ == '__main__':
     
     n = len(sys.argv)
     if (n-1 > 0):
-        if exists(sys.argv[1]):
+        if isfile(path)(sys.argv[1]):
             file_operations.openfilename(root, sys.argv[1])
         for i in range(2,n):
-            if exists(sys.argv[i]):
+            if isfile(sys.argv[i]):
                 tabs.add_tab(root)
                 file_operations.openfilename(root, sys.argv[i])
     else:
         pass
     
-    root.mainloop()
-        
+    root.mainloop()        
