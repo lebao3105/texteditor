66c1a343ea8492a289fceb77c8fc8f4fc9a7bb50
lebao3105 <bao12345yocoo@gmail.com>
2022-08-04 13:00:51 +0700

    Complety rewritten the module (editvalue not completed yet)

--- texteditor/miscs/get_config.py
+++ texteditor/miscs/get_config.py
@@ -4,11 +4,33 @@ import configparser
 import platform
 
 if platform.system() == "Windows":
-    dir = os.environ['USERPROFILE'] + "\\.config\\"
+    dir = os.environ['USERPROFILE'] + "\\.config\\texteditor_config.ini"
     defconsole = 'cmd'
 else:
     dir = os.environ['HOME'] + "/.config/"
     defconsole = 'xterm'
+
+cfg = configparser.ConfigParser()
+cfg.read(dir+"config.ini")
+
+# Default variables.
+# We must use cfg.get() to get the current variable's value.
+cfg['global'] = {
+    'color': 'light',
+    'sub_color': 'default',
+    'font': 'default'
+}
+
+cfg['popups'] = {
+    # TODO: They should be different
+    'width': str(constant.DEFAULT_OTHERS_WIGHT),
+    'height': str(constant.DEFAULT_OTHERS_WIGHT)
+}
+
+cfg['cmd'] = {
+    'defconsole': defconsole,
+    'isenabled': 'yes'
+}
 
 def check_exists():
     if os.path.isdir(dir):
@@ -16,57 +38,55 @@ def check_exists():
             return True
         else:
             with open(dir + "config.ini", "w") as configfile:
-                configfile.write("[global]\n")
-                configfile.write("color = light\n")
-                configfile.write("sub_color = default\n")
-                configfile.write("font = default\n\n")
-                configfile.write("[other_windows]\n")
-                configfile.write("width = "+str(constant.DEFAULT_OTHERS_WIGHT)+"\n")
-                configfile.write("height = "+str(constant.DEFAULT_OTHERS_WIGHT)+"\n\n")
-                configfile.write("[cmd]\n")
-                configfile.write("defconsole = "+defconsole+"\n")
-                configfile.write("isenabled = no")
+                configfile.write(cfg)
     else:
-        test = os.mkdir(dir)
-        if not test:
-            raise Exception("Could not create the configure directory!")
+        try:
+            os.mkdir(dir)
+        except:
+            raise "Could not create "+dir
 
-def check_if_its_a_window(self):
-    if self.winfo_class() == "Tk":
-        return "Tk"
-    elif self.winfo_class() == "Toplevel":
-        return "Toplevel"
-    elif self.winfo_class() == "Frame":
-        return "Frame"
-    elif self.winfo_class() == "Label" or self.winfo_class() == "Button" or self.winfo_class() == "Text":
+
+def find_widget(self):
+    arr = [
+        'Tk', 'Frame', 'TopLevel'
+    ]
+
+    if self.winfo_class() == "Label" or self.winfo_class() == "Button" or self.winfo_class() == "Text":
         return "Text"
-    else:
-        return False
-
-# At least we made this independently from set_window_color(self).
-def change_text_color(self):
-    check_exists()
-    with open(dir + "config.ini", "r") as configfile:
-        parser = configparser.ConfigParser()
-        parser.read_file(configfile)
-        if parser.get("global", "color") == "dark":
-            item = constant.DARK_BG
-        else:
-            item = constant.LIGHT_BG
-
-        if parser.get("global", "sub_color") == "default":
-            return True
-        elif parser.get("global", "sub_color") == "Green":
-            if check_dark_mode(self, item):
-                self.configure(fg=constant.GREEN_TEXT, bg=item)
-        elif parser.get("global", "sub_color") == "Blue":
-            if check_dark_mode(self, item):
-                self.configure(fg=constant.BLUE_TEXT, bg=item)
-        elif parser.get("global", "sub_color") == "Red":
-            if check_dark_mode(self, item):
-                self.configure(fg=constant.RED_TEXT, bg=item)
+    for item in arr:
+        if self.winfo_class() == item:
+            return item
         else:
             return False
+
+# At least we made this independent from set_window_color(self).
+def change_text_color(self):
+    """Get text color defined in global->sub_color.
+    Supported colors:
+    default: default color
+    Green: green color
+    Blue: blue color
+    Red: red color
+    If check_dark_mode return(s) False, any other color then default won't be applied."""
+    check_exists()
+    if cfg.get("global", "color") == "dark":
+        item = constant.DARK_BG
+    else:
+        item = constant.LIGHT_BG
+
+    if cfg.get("global", "sub_color") == "default":
+        return True
+    elif cfg.get("global", "sub_color") == "Green":
+        if check_dark_mode(self, item):
+            self.configure(fg=constant.GREEN_TEXT, bg=item)
+    elif cfg.get("global", "sub_color") == "Blue":
+        if check_dark_mode(self, item):
+            self.configure(fg=constant.BLUE_TEXT, bg=item)
+    elif cfg.get("global", "sub_color") == "Red":
+        if check_dark_mode(self, item):
+            self.configure(fg=constant.RED_TEXT, bg=item)
+    else:
+        return False
 
 # Check if we are in dark mode which allows us 
 # to change the text color
@@ -80,17 +100,14 @@ def check_dark_mode(self, color):
 def set_window_color(self):
     check_exists()
 
-    with open(dir + "config.ini", "r") as configfile:
-        config = configparser.ConfigParser()
-        config.read_file(configfile)
-        if config["global"]["color"] == "dark":
-            change_color(self, 'dark')
-        elif config["global"]["color"] == "light":
-            change_color(self, 'light')
+    if cfg["global"]["color"] == "dark":
+        change_color(self, 'dark')
+    elif cfg["global"]["color"] == "light":
+        change_color(self, 'light')
     
     # For TopLevel windows
-    if check_if_its_a_window(self) == "Toplevel":
-        self.geometry(config["other_windows"]["width"] + "x" + config["other_windows"]["height"])
+    if find_widget(self) == "Toplevel":
+        self.geometry(cfg["other_windows"]["width"] + "x" + cfg["other_windows"]["height"])
         self.resizable(False, False)
         self.grab_set()
 
@@ -111,14 +128,18 @@ def change_color(self, color):
     else:
         self.config(bg=item)
     
-    if check_if_its_a_window(self) == "Text":
+    if find_widget(self) == "Text":
         self.configure(bg=item, fg=sub_item)
 
 # Get a value...
 def getvalue(section:str, name:str):
     check_exists()
-    with open(dir + "config.ini") as f:
-        config = configparser.ConfigParser()
-        config.read(f)
-        return config[section][name]
+    if not section in cfg:
+        raise "Section not found "+section
+    else:
+        return cfg.get(section, name)
+
+def editvalue(section:str, name:str, value:str):
+    check_exists()
+        
         
