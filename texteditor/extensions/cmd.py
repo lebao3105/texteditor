865802be84f947a5fb8afee2eb3e5c54be633946
lebao3105 <bao12345yocoo@gmail.com>
2022-08-04 13:03:18 +0700

    Run tasks on the background with threading.Thread

--- texteditor/extensions/cmd.py
+++ texteditor/extensions/cmd.py
@@ -1,8 +1,7 @@
-import os
-import sys
+import os, sys, subprocess
+from signal import SIGTERM
 from tkinter import *
-import subprocess
-from tkinter.messagebox import showwarning
+from threading import Thread
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from miscs import get_config
 
@@ -11,25 +10,28 @@ class cmd(Text):
     def __init__(self, master):
         super().__init__(master)
         self._ = master._
-        self.configure(fg='white', bg='black')
-        self.bind('<Return>', self.runcommand)
+        #self.configure(fg='white', bg='black')
+        self.bind('<Return>', self.checkcmds)
         self.pack(expand=True, fill="both")
         self.warning()
-        master.commandprompt = self
+        self.parent = master
 
-    """
-    Run a command.
-    Outputs will be redirected to the widget.
-    """
-    def runcommand(self, *args):
+    def checkcmds(self, *args):
         command = self.get(1.0, END).split('\n')[-2]
         append = self.insert
 
         if command == 'exit':
-            self.destroy()
-        elif command== 'clear':
+            try:
+                os.killpg(self.result.pid, SIGTERM)
+            except ProcessLookupError:
+                pass
+            print('Closed Console Window.')
+            self.master.destroy() # Destroy the Toplevel widget
+
+        elif command == 'clear':
             self.delete(1.0, END)
-            self.mark_set("insert", "-1.-1")
+            self.mark_set("insert", "%d.%d" % (1.0, 1.0))
+
         elif command == 'help':
             append('end', "\n")
             append('end', self._("Available commands:\n"))
@@ -37,28 +39,30 @@ class cmd(Text):
             append('end', self._("help: Show this message\n"))
             append('end', self._("clear: Clear this box\n"))
             append('end', self._("term: Open terminal (not this one) - don't run this if you are running this application from console!\n"))
-            append('end', self._("Running application which requires user input is not recommended yet.\n"))
+            append('end', self._("Running CONSOLE application which requires user input is not recommended yet.\n"))
+            append('end', self._("You won't get anything.\n"))
+
         elif command == 'term':
+            append('end', '\n')
             append('end', self._("This will open console defined by defconsole value in configuration file. The application will be temporarity unusable!"))
             os.system(get_config.getvalue('cmd', 'defconsole'))
-        else:
-            command_splited = command.split(' ')
-            result = subprocess.run(command_splited, shell=True,
-                        stderr=subprocess.STDOUT, stdout=subprocess.PIPE, env=os.environ)
-            output = result.stdout
-            append('end', '\n')
-            append('end', output)
 
-    def warning(self):
-        showwarning(
-            title=self._('Warning'),
-            message=self._("""This is a working feature - use some commands will make the application unusable. Run help to get more notes and available commands!""")
-        )
+        else:
+            rec_thread = Thread(target = lambda: self.runcommand(command))
+            rec_thread.start()
+
+    def runcommand(self, cmd : str):
+        self.result = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid,
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = self.result.communicate()
+        self.insert('end', output)
+        self.insert('end', error)
+        self.result.wait()
 
 class CommandPrompt(Toplevel):
 
     def __init__(self, master, event=None):
         super().__init__(master)
         self._ = master._
-        self.title("Command Prompt - Tkinter")
+        self.title("Command Window")
         cmd(self)
