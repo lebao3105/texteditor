0d9e5574ce30cd7859514c422db1a68809876048
lebao3105 <bao12345yocoo@gmail.com>
2022-08-30 18:42:21 +0700

    Run texteditor without using python -m

--- texteditor/__init__.py
+++ texteditor/__init__.py
@@ -1,8 +1,31 @@
-from . import __main__
+import gettext
 import sys
+from os.path import isfile
 
-def start():
-    return __main__.start_app(sys.argv)
+import texteditor.mainwindow
+import texteditor.tabs
+from texteditor.miscs import file_operations
 
-if __name__ == '__main__':
-    start()
+gettext.bindtextdomain("base", "po")
+gettext.textdomain("base")
+_ = gettext.gettext
+
+
+def start_app(argv: sys.argv):
+    root = texteditor.mainwindow.MainWindow(_)
+    n = len(argv)
+    if n - 1 > 0:
+        if isfile(argv[1]):
+            file_operations.openfilename(root, argv[1])
+        for i in range(2, n):
+            if isfile(argv[i]):
+                texteditor.tabs.add_tab(root)
+                file_operations.openfilename(root, argv[i])
+    else:
+        pass
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    start_app(sys.argv)
--- texteditor/__main__.py
+++ texteditor/__main__.py
@@ -1,23 +1,6 @@
 import sys
-import texteditor.tabs
-import texteditor.mainwindow
-from texteditor.miscs import file_operations
-from os.path import isfile
 
-def start_app(argv:sys.argv):  
-    root = texteditor.mainwindow.MainWindow()
-    n = len(argv)
-    if (n-1 > 0):
-        if isfile(argv[1]):
-            file_operations.openfilename(root, argv[1])
-        for i in range(2,n):
-            if isfile(argv[i]):
-                texteditor.tabs.add_tab(root)
-                file_operations.openfilename(root, argv[i])
-    else:
-        pass
-    
-    root.mainloop()
+from texteditor import start_app
 
-if __name__ == '__main__':
-    sys.exit(start_app(sys.argv))
+if __name__ == "__main__":
+    start_app(sys.argv)
