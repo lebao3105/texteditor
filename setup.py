e344392f514950eda98a3a0eb6f49e5724f3c3ea
lebao3105 <bao12345yocoo@gmail.com>
2022-09-16 12:35:09 +0700

    Version 1.3

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ long_des = (currdir / "README.md").read_text(encoding="utf8")
 setup(
     name="texteditor",
     author="Le Bao Nguyen",
-    version="1.3.rc",
+    version="1.3",
     url="https://github.com/lebao3105/texteditor",
     description="A Tkinter text editor",
-    license="GPL v3",
+    license="GPL V3",
     long_description=long_des,
     long_description_content_type="text/markdown",
     install_requires=["configparser"],
@@ -18,4 +18,14 @@ setup(
     include_package_data=True,
     packages=find_packages(),
     package_data={"texteditor": ["icons/*.png", "../po/*/LC_MESSAGES/*.mo"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Text Editors",
+    ],
 )
