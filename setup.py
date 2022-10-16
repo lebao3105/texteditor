54a43041bb798048e3dc021dba04d400a8851d9f
lebao3105 <bao12345yocoo@gmail.com>
2022-10-16 16:38:25 +0700

    Version 1.4-dev

--- setup.py
+++ setup.py
@@ -7,10 +7,9 @@ long_des = (currdir / "README.md").read_text(encoding="utf8")
 setup(
     name="texteditor",
     author="Le Bao Nguyen",
-    version="1.3",
+    version="1.4-dev",
     url="https://github.com/lebao3105/texteditor",
     description="A Tkinter text editor",
-    license="GPL v3",
     long_description=long_des,
     long_description_content_type="text/markdown",
     install_requires=["configparser", "darkdetect", "sv_ttk"],
