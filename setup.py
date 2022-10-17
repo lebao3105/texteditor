2a50c65bfb9e2cc0278bae2b4eb3eda05be4b95d
lebao3105 <bao12345yocoo@gmail.com>
2022-10-17 17:39:52 +0700

    The About window is back!

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description="A Tkinter text editor",
     long_description=long_des,
     long_description_content_type="text/markdown",
-    install_requires=["configparser", "darkdetect", "sv_ttk"],
+    install_requires=["configparser", "darkdetect", "sv_ttk", "pygubu"],
     entry_points={"gui_scripts": ["texteditor = texteditor:start_app"]},
     include_package_data=True,
     packages=find_packages(),
@@ -20,7 +20,7 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
