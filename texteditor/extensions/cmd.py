6d2e9dae5c1a318229f14537a1d80d146628f911
lebao3105 <bao12345yocoo@gmail.com>
2022-12-11 14:32:44 +0700

    cd command

--- texteditor/extensions/cmd.py
+++ texteditor/extensions/cmd.py
@@ -1,14 +1,12 @@
-import gettext
 import os
+import signal
 import subprocess
-import sys
+import texteditor
 import threading
-from signal import SIGTERM
-from tkinter import END, BooleanVar, Toplevel, messagebox
 
+from tkinter import END, BooleanVar, Toplevel, messagebox
 from texteditor.backend import get_config, textwidget
 
-_ = gettext.gettext
 arr = []
 
 
@@ -16,16 +14,19 @@ class cmd(textwidget.TextWidget):
     useUnRedo = True
     useWrap = True
 
-    def __init__(self, master, **kw):
-        super().__init__(master, **kw)
+    def __init__(self, master, _=None, **kw):
+        super().__init__(master, _=_, **kw)
         self.parent = master
         self.bind("<Return>", self.checkcmds)
 
         self.readonlybtn = BooleanVar()
         self.readonlybtn.set(False)
         self.addMenucheckbtn(
-            label=_("Read only"), fn=self.readonlymode, variable=self.readonlybtn
+            label=self._("Read only"), fn=self.readonlymode, variable=self.readonlybtn
         )
+        self.configure(foreground="white", background="black", insertbackground="white")
+        self.statusbar.righttext.configure(text="")
+        self.statusbar.writeleftmessage(texteditor.currdir)
 
     def checkcmds(self, *args):
         command = self.get(1.0, END).split("\n")[-2]
@@ -33,12 +34,8 @@ class cmd(textwidget.TextWidget):
 
         if command == "exit":
             try:
-                # TODO: Manage processes
-                if sys.platform == "win32":
-                    self.result.kill()
-                else:
-                    for pids in arr:
-                        os.killpg(pids, SIGTERM)
+                for pids in arr:
+                    os.kill(pids, signal.SIGTERM)
             except ProcessLookupError:
                 pass
             print("Closed Console Window.")
@@ -49,35 +46,45 @@ class cmd(textwidget.TextWidget):
 
         elif command == "help":
             append("end", "\n")
-            append("end", _("Available commands:\n"))
-            append("end", _("exit: Close this box\n"))
-            append("end", _("help: Show this message\n"))
-            append("end", _("clear: Clear this box\n"))
+            append("end", self._("Available commands:\n"))
+            append("end", self._("exit: Close this box\n"))
+            append("end", self._("help: Show this message\n"))
+            append("end", self._("clear: Clear this box\n"))
             append(
                 "end",
-                _(
+                self._(
                     "term: Open terminal (not this one) - don't run this if you are running this application from console!\n"
                 ),
             )
             append(
                 "end",
-                _(
+                self._(
                     "Running CONSOLE application which requires user input is not recommended yet.\n"
                 ),
             )
-            append("end", _("You won't get anything.\n"))
+            append("end", self._("You won't get anything.\n"))
 
         elif command == "term":
             append("end", "\n")
             append(
                 "end",
-                _(
+                self._(
                     "This will open console defined by defconsole value in configuration file. Please don't run any shell!"
                 ),
             )
             cmd = get_config.GetConfig.getvalue("cmd", "defconsole")
 
-            if cmd == ("bash" or "xonsh" or "zsh" or "sh"):
+            if cmd == (
+                "bash"
+                or "xonsh"
+                or "zsh"
+                or "fish"
+                or "sh"
+                or "pwsh"
+                or "powershell"
+                or "tmux"
+                or "kgx"
+            ):
                 messagebox.showerror(
                     title="Error",
                     message="This application cannot run shell, use terminal emulator instead.\nProgram: "
@@ -88,7 +95,12 @@ class cmd(textwidget.TextWidget):
             else:
                 threading.Thread(target=lambda: self.runcommand(cmd)).start()
         elif command.startswith("cd "):
-            append("end", _("You cannot use cd in this environment."))
+            try:
+                os.chdir(command.removeprefix("cd "))
+            except OSError:
+                append("end", self._("No such a directory like that"))
+            else:
+                self.statusbar.writeleftmessage(os.getcwd())
         else:
             rec_thread = threading.Thread(target=lambda: self.runcommand(command))
             rec_thread.start()
@@ -114,15 +126,13 @@ class cmd(textwidget.TextWidget):
 
 
 class CommandPrompt(Toplevel):
-    def __init__(self, master, event=None):
+    def __init__(self, master, _=None, event=None):
         super().__init__(master)
         self.geometry("600x400")
-        self.title(_("Command Window"))
+        self.title("Commander")
         console = cmd(
             self,
-            foreground="white",
-            background="black",
-            insertbackground="white",
+            _=_,
             undo=False,
         )
         console.pack(expand=True, fill="both")
