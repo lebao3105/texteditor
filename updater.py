e632a4f1534052787f208adb5ef37e8fb0a1179c
lebao3105 <bao12345yocoo@gmail.com>
2023-11-12 22:34:26 +0700

    Going to implement a new update checker (thanks to OCLP)

--- updater.py
+++ updater.py
@@ -1,11 +1,11 @@
 import configparser
 import json
 import logging
+import sys
 import packaging.version as version
 import requests
 
-GITHUB_LINK = "https://github.com/lebao3105/texteditor"
-RAW = "https://raw.githubusercontent.com/lebao3105/texteditor"
+GITHUB_LINK = "https://api.github.com/repos/lebao3105/texteditor"
 RELEASES = GITHUB_LINK + "/releases/tag/"
 
 currver: str
@@ -24,40 +24,38 @@ def parse_json(inipath: str):
     If the file cannot be reached, will return request.get.status_code.
     """
 
-    r = requests.get(RAW + "/data/version.json")
-    if not r.text:
-        return r.status_code
-
-    if not json.loads(r.text): return "invalid_json"
-
     parser = configparser.ConfigParser()
     parser.read(inipath, "utf-8")
 
+    # Get the current app type & branch
     try:
         branch = parser["base"]["branch"]
     except KeyError:
-        logger.info("No app versioning setting found. Will assume that we'll go stable.")
+        logger.info("No app versioning setting found. Will go stable.")
         branch = "latest"
     finally:
         global currappbranch, currver
-        if version.Version(currver) < version.Version ("1.6"): # Tkinter
+        if version.parse(currver) < version.parse("1.6"): # Tkinter
             currappbranch = "tk"
         else:
             currappbranch = "wx"
         branch += f"-{currappbranch}"
     
-    try:
-        target_branch = json.loads(r.text)[branch]
-    except:
-        logging.warning("""Invalid branch name on settings.ini? Or version.json?
-                        Whatever the updater must be stopped.
-                        """
-        )
-        return "invalid_json: No such branch %s found in version.json" % branch
-    else:
-        if version.Version(currver) < version.Version(target_branch["version"]):
-            return target_branch["version"], branch, target_branch["changelog"]
-        else: return # Up to date
+    # Get the latest release
+    r = requests.get(GITHUB_LINK + "/releases")
+    all = json.loads(r.text)
+
+    latest = all[0]
+    tag = latest["tag_name"].removeprefix("v")
+
+    # TODO
+    for i in range(latest["assets"]):
+        if latest["assets"][i]["name"].startswith(f"textworker-{latest}-{sys.platform}"):
+            get = latest["assets"][i]["browser_download_url"]
+
+    if version.parse(tag) > version.parse(currver):
+        return tag, latest["body"], get
+
 
 # def install(version: str):
 #     r = requests.get()
